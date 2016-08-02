18053d4abb0bd8ead20ec43a5e8bf507f3e7eec0
Steven <steven.gpa@gmail.com>
2016-08-02 08:55:43 -0600

    States code were improved

--- public/src/js/states/login.js
+++ public/src/js/states/login.js
@@ -5,70 +5,42 @@ import {isEmail} from 'validator';
 const CODE_LEN = 10;
 
 export default observable({
+	// Observables:
 	email: '',
 	code: '',
-	disabled: true,
-	formEmailState: undefined,
-	formCodeState: undefined,
-	isEmailOK: false,
-	isCodeOK: false,
+	// Computeds:
+	disabled() {
+		return !(this.isEmailOK && this.isCodeOK);
+	},
+	formEmailState() {
+		if (isEmail(this.email) && _.size(this.email) > 0) {
+			return 'success';
+		} else if (!isEmail(this.email) && _.size(this.email) > 0) {
+			return 'error';
+		} else if (_.size(this.email) === 0) {
+			return undefined;
+		}
+	},
+	formCodeState() {
+		if (_.size(this.code) === CODE_LEN) {
+			return 'success';
+		} else if (_.size(this.code) < CODE_LEN && this.code !== '' || _.size(this.code) > CODE_LEN) {
+			return 'error';
+		} else if (_.size(this.code) === 0) {
+			return undefined;
+		}
+	},
+	isEmailOK() {
+		return (isEmail(this.email) && _.size(this.email) > 0);
+	},
+	isCodeOK() {
+		return (_.size(this.code) === CODE_LEN);
+	},
+	// Actions:
 	setEmail: action(function (status) {
 		this.email = status;
-		this.checkEmail();
-	}),
-	checkEmail: action(function() {
-		// Email
-		if (isEmail(this.email) && _.size(this.email) > 0) {
-			this.isEmailOK = true;
-			this.formEmailState = 'success';
-
-		} else if (!isEmail(this.email) && _.size(this.email) > 0) {
-			this.isEmailOK = false;
-			this.formEmailState = 'error';
-
-		} else if (_.size(this.email) === 0) {
-			this.formEmailState = undefined;
-			this.isEmailOK = false;
-		}
-
-		this.disabled = !(this.isEmailOK && this.isCodeOK);
 	}),
 	setCode: action(function (status) {
 		this.code = status;
-		this.checkCode();
-	}),
-	checkCode: action(function() {
-		// Code
-		if (_.size(this.code) === CODE_LEN) {
-			this.isCodeOK = true;
-			this.formCodeState = 'success';
-
-		} else if (_.size(this.code) < CODE_LEN && this.code !== '' || _.size(this.code) > CODE_LEN) {
-			this.isCodeOK = false;
-			this.formCodeState = 'error';
-
-		} else if (_.size(this.code) === 0) {
-			this.formCodeState = undefined;
-			this.isCodeOK = false;
-		}
-
-		this.disabled = !(this.isEmailOK && this.isCodeOK);
 	})
 });
-
-/*
- setEmail: action(function setEmail(status) {
- this.email = status;
-
- if (isEmail(this.email) && _.size(this.email) > 0) {
- this.disabled = false;
- this.formState = 'success';
-
- } else if (!isEmail(this.email) && _.size(this.email) > 0) {
- this.disabled = true;
- this.formState = 'error';
-
- } else if (_.size(this.email) === 0) {
- this.formState = undefined;
- }
- */
--- public/src/js/states/signup.js
+++ public/src/js/states/signup.js
@@ -3,22 +3,23 @@ import {observable, action} from 'mobx';
 import {isEmail} from 'validator';
 
 export default observable({
+	// Observables:
 	email: '',
-	disabled: true,
-	formState: undefined,
+	// Computeds:
+	disabled() {
+		return !isEmail(this.email);
+	},
+	formState() {
+		if (isEmail(this.email) && _.size(this.email) > 0) {
+			return 'success';
+		} else if (!isEmail(this.email) && _.size(this.email) > 0) {
+			return 'error';
+		} else if (_.size(this.email) === 0) {
+			return undefined;
+		}
+	},
+	// Actions:
 	setEmail: action(function setEmail(status) {
 		this.email = status;
-
-		if (isEmail(this.email) && _.size(this.email) > 0) {
-			this.disabled = false;
-			this.formState = 'success';
-
-		} else if (!isEmail(this.email) && _.size(this.email) > 0) {
-			this.disabled = true;
-			this.formState = 'error';
-
-		} else if (_.size(this.email) === 0) {
-			this.formState = undefined;
-		}
 	})
 });
